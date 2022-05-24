2aa6b5fa582e2feabdf23d2bd2a71241e80b55aa
Melony <105548720+hjz1010@users.noreply.github.com>
2022-05-24 09:42:01 +0900

    5/24

--- JS.js
+++ JS.js
@@ -1,7 +1,6 @@
 const content = "안녕하세요! \n wecode 35기 홍현진의 자기소개 페이지 입니다:)";
 const text = document.querySelector(".introduce");
 let i = 0;
-
 function typing() {
   let txt = content[i++];
   text.innerHTML += txt === "\n" ? "<br>": txt;
