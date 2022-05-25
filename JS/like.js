b90263522712ff4116f97fb10d9727f8d38b146e
Melony <105548720+hjz1010@users.noreply.github.com>
2022-05-25 09:17:27 +0900

    5/25

---
+++ JS/like.js
@@ -0,0 +1,30 @@
+//JS로 이미지 슬라이드쇼 효과
+
+// var slideIndex = 0;
+// showSlides();
+//
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     // var dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//        slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     // for (i = 0; i < dots.length; i++) {
+//     //     dots[i].className = dots[i].className.replace(" active", "");
+//     // }
+//     slides[slideIndex-1].style.display = "block";
+//     // dots[slideIndex-1].className += " active";
+//     setTimeout(showSlides, 4000); // Change image every 2 seconds
+// }
+// /* 슬라이드쇼를 위한 CSS 설정 */
+// .slideshow-container {
+//   max-height: 500px;
+//   position: relative;
+//   margin: auto;
+// }
+// .mySlides {
+//   display: none;
+// }
