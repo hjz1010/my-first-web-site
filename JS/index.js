95567eba32f2cc3677833fcf3491bfb77f630677
Melony <105548720+hjz1010@users.noreply.github.com>
2022-05-26 07:58:53 +0900

    5/26

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+// 타이핑 효과 만들기
 const content = "안녕하세요! \n wecode 35기 홍현진의 자기소개 페이지 입니다:)";
 const text = document.querySelector(".introduce");
 let i = 0;
@@ -9,5 +10,7 @@ function typing() {
     i = 0;
   }
 }
-
 setInterval (typing, 100);
+// setInterval은 일정한 주기로 함수를 반복
+// setTimeout은 일정한 시간이 지난 후 함수를 시행
+//  재귀함수로 작성하면 반복 시행을 만들 수 있다.
