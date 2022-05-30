2005e58700facfb8afa89dacb9ed81754bea5867
Melony <105548720+hjz1010@users.noreply.github.com>
2022-05-31 00:08:30 +0900

    타이핑 효과 수정
     1. 문장 완성 후 잠시 멈추기 
      - 문장 뒤에 공백을 넣어서 구현
     2. clearInterval 사용해서 5회 반복 후 멈추도록 
      - 그 과정에서 반복이 끝나면 문장 끝에 undefined가 출력되는 오류 발생 >> 문장을 초기화하는 조건문의 조건부분에 오류를 발견하고 해결!

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,28 @@
 // 타이핑 효과 만들기
-const content = "안녕하세요! \n wecode 35기 홍현진의 자기소개 페이지 입니다:)";
+const content = "안녕하세요! \n wecode 35기 홍현진의 자기소개 페이지 입니다:) \n          ";
 const text = document.querySelector(".introduce");
 let i = 0;
+let count = 1;  // clearInterval로 반복을 멈추기 위해 실행횟수를 카운팅
+
 function typing() {
   let txt = content[i++];
   text.innerHTML += txt === "\n" ? "<br>": txt;
-  if (i >content.length) {
-    text.textContent = "";
-    i = 0;
+  if (i >= content.length - 1) {
+    if (count === 5){    // 원하는 카운트 횟수 도달 시 clearInterval 시행
+      clearInterval(intervalID);
+      // console.log(clearInterval(intervalID));  //Output: undefined
+      // 근데 문제는 이게 왜 웹페이지에 출력이 되는것인가!!!!
+      // 조건이 i > content.length 라서.. 반복 마지막에 txt값이 없는 오류가 있었다. 그래서 아무거나 등장한 값을 txt에 넣고 출력한 듯!
+    }
+    else {
+      text.textContent = "";
+      i = 0;
+      count++;   // 한 바퀴 반복 후 count+1
+    }
   }
 }
-setInterval (typing, 100);
+
+let intervalID = setInterval(typing, 100);
 // setInterval은 일정한 주기로 함수를 반복
 // setTimeout은 일정한 시간이 지난 후 함수를 시행
 //  재귀함수로 작성하면 반복 시행을 만들 수 있다.
